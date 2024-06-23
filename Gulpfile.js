a1c3d20a505efec763bd7286144afcbbe0d4d837
makamat <maka.mat@proton.me>
2024-06-23 11:37:28 +0200

    added gulp-imagemin

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,20 @@
 import gulp from "gulp";
 import { rimraf } from "rimraf";
+import gulpImageMin from "gulp-imagemin";
 
 const clean = async () => {
     return rimraf("dist/");
+};
+
+const processImages = async () => {
+    return gulp
+        .src("src/assets/images/**/*.{jpg,png}", { removeBOM: false })
+        .pipe(gulpImageMin())
+        .pipe(gulp.dest("dist/assets/images/"));
 };
 
 const build = async () => {
     return console.log("test");
 };
 
-export { clean, build as default };
+export { clean, processImages as "process-images", build as default };
