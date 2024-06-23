e8e37c5eede71f606704b7ce1780506ebe7cfd94
makamat <maka.mat@proton.me>
2024-06-23 11:26:24 +0200

    added rimraf module

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,12 @@
 import gulp from "gulp";
+import { rimraf } from "rimraf";
+
+const clean = async () => {
+    return rimraf("dist/");
+};
 
 const build = async () => {
     return console.log("test");
 };
 
-export { build as default };
+export { clean, build as default };
