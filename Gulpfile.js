d9e4dd32a6772fca6aaf8aa1ebf3e795141cf3fe
makamat <maka.mat@proton.me>
2024-06-23 16:06:47 +0200

    refactored gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,43 +6,65 @@ import gulpCleanCss from "gulp-clean-css";
 import gulpAutoprefixer from "gulp-autoprefixer";
 import gulpConcat from "gulp-concat";
 
-const processCss = async () => {
-    return gulp
-        .src("src/assets/css/**/*.css", { sourcemaps: true })
-        .pipe(gulpAutoprefixer())
-        .pipe(gulpCleanCss())
-        .pipe(gulpConcat("styles.css"))
-        .pipe(gulp.dest("dist/assets/css/", { sourcemaps: "." }));
-};
+const paths = {
+    html: {
+        src: "src/index.html",
+        dest: "dist/",
+    },
 
-const clean = async () => {
-    return rimraf("dist/");
-};
+    css: {
+        src: "src/assets/css/**/*.css",
+        dest: "dist/assets/css/",
+    },
 
-const processImages = async () => {
-    return gulp
-        .src("src/assets/images/**/*.{jpg,png}", { removeBOM: false })
-        .pipe(gulpImageMin())
-        .pipe(gulp.dest("dist/assets/images/"));
+    images: {
+        src: "src/assets/images/**/*.{jpg,png}",
+        dest: "dist/assets/images/",
+    },
+
+    fonts: {
+        src: "src/assets/fonts/**/*.woff2",
+        dest: "dist/assets/fonts/",
+    },
 };
 
 const processHtml = async () => {
-    const minifOptions = {
+    const minifyOptions = {
         removeComments: true,
         collapseWhitespace: true,
         removeOptionalTags: true,
     };
 
     return gulp
-        .src("src/index.html", { sourcemaps: true })
-        .pipe(gulpHtmlMin(minifOptions))
-        .pipe(gulp.dest("dist/", { sourcemaps: "." }));
+        .src(paths.html.src)
+        .pipe(gulpHtmlMin(minifyOptions))
+        .pipe(gulp.dest(paths.html.dest));
+};
+
+const processCss = async () => {
+    return gulp
+        .src(paths.css.src, { sourcemaps: true })
+        .pipe(gulpConcat("styles.css"))
+        .pipe(gulpAutoprefixer())
+        .pipe(gulpCleanCss())
+        .pipe(gulp.dest(paths.css.dest, { sourcemaps: "." }));
+};
+
+const processImages = async () => {
+    return gulp
+        .src(paths.images.src, { removeBOM: false })
+        .pipe(gulpImageMin())
+        .pipe(gulp.dest(paths.images.dest));
 };
 
 const processFonts = async () => {
     return gulp
-        .src("src/assets/fonts/**/*.woff2", { removeBOM: false })
-        .pipe(gulp.dest("dist/assets/fonts/"));
+        .src(paths.fonts.src, { removeBOM: false })
+        .pipe(gulp.dest(paths.fonts.dest));
+};
+
+const clean = async () => {
+    return rimraf("dist/");
 };
 
 const build = gulp.series(
